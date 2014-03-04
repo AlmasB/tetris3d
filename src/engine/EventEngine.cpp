796290286dae7c8b207e7b86fb88ba5c5d9fc8f9
AlmasB <toll.everon@gmail.com>
2014-03-04 23:40:55 +0000

    simplified controller cmd parsing

--- src/engine/EventEngine.cpp
+++ src/engine/EventEngine.cpp
@@ -1,6 +1,6 @@
 #include "EventEngine.h"
 
-EventEngine::EventEngine() : running(true), androidCtrlEnabled(false) {
+EventEngine::EventEngine() : running(true), androidCtrlEnabled(true) {
 	for (uint i = 0; i < Key::LAST; ++i) {
 		keys[i] = false;
 	}
@@ -65,43 +65,29 @@ void EventEngine::runConnThread() {
 		else {
 			remoteip = SDLNet_TCP_GetPeerAddress(client);	// TODO: do init once and redo on connection lost
 			ipaddr = SDL_SwapBE32(remoteip->host);
-			len = SDLNet_TCP_Recv(client, message, 1024);
+			len = SDLNet_TCP_Recv(client, message, 1024);	// doesn' return until catches a tcp packet
+
+#ifdef __DEBUG
+			std::cout << "Received: " << len << " bytes" << std::endl;
+#endif
 
 			for (int i = 0; i < len; ++i) {
 				char c = (int)message[i];
+			
+				keys[Key::W] = ('W' == c && !keys[Key::W]);
+				keys[Key::S] = ('S' == c && !keys[Key::S]);
+				keys[Key::A] = ('A' == c && !keys[Key::A]);
+				keys[Key::D] = ('D' == c && !keys[Key::D]);
 
-				if ('W' == c && !keys[Key::W]) {	// TODO: we don't really want that, do smth smoother
-					keys[Key::W] = true;
-				}
-				else {
-					keys[Key::W] = false;
-				}
-
-				if ('S' == c && !keys[Key::S]) {
-					keys[Key::S] = true;
-				}
-				else {
-					keys[Key::S] = false;
+				if ('N' == c) {
+					for (int i = 0; i < Key::LAST; ++i)
+						keys[i] = false;
 				}
 
-				if ('A' == c && !keys[Key::A]) {
-					keys[Key::A] = true;
-				}
-				else {
-					keys[Key::A] = false;
-				}
-
-				if ('D' == c && !keys[Key::D]) {
-					keys[Key::D] = true;
-				}
-				else {
-					keys[Key::D] = false;
-				}
-
-
+				std::cout << c << std::endl;
 			}
 		}
-		SDL_Delay(100);
+		SDL_Delay(16);	// TODO: too specific, design flaw, need to isolate the engine side
 	}
 
 #ifdef __DEBUG
@@ -122,8 +108,6 @@ void EventEngine::pollEvents() {
 		buttons[Mouse::BTN_LEFT] = !((SDL_GetMouseState(NULL, NULL) & SDL_BUTTON(SDL_BUTTON_LEFT)) == 0);
 		buttons[Mouse::BTN_RIGHT] = !((SDL_GetMouseState(NULL, NULL) & SDL_BUTTON(SDL_BUTTON_RIGHT)) == 0);
 	}
-
-
 }
 
 void EventEngine::updateKeys(const SDL_Keycode &key, bool keyDown) {
