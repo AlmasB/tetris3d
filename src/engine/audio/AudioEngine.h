044a0a99f09f4119d15ced1f688dd2a7e7db4d86
AlmasB <toll.everon@gmail.com>
2014-05-27 23:49:46 +0100

    added sound volume control

--- src/engine/audio/AudioEngine.h
+++ src/engine/audio/AudioEngine.h
@@ -10,10 +10,27 @@ class AudioEngine {
 	private:
 		AudioEngine();
 		bool soundOn;
+		int volume;
 	public:
 		~AudioEngine();
 		void toggleSound();
+
+		/**
+		* Controls the volume of all sounds
+		* @param volume - in the range [0..128]
+		*/
+		void setSoundVolume(const int &);
+		int getSoundVolume();
+
 		void playSound(Mix_Chunk * sound);
+
+		/**
+		* Call this to manually specify the volume of the sound
+		*
+		* @param sound - the sound to play
+		* @param volume - the volume at which to play in range [0..128]
+		*/
+		void playSound(Mix_Chunk * sound, const int & _volume);
 };
 
 #endif
